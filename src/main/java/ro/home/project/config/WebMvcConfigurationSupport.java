cab75e4d9db3d6a3280846a825e8f0c84bbb9ff3
Marian <mdanscoi@gmail.com>
2022-04-13 17:39:57 +0300

    update project

--- src/main/java/ro/home/project/config/WebMvcConfigurationSupport.java
+++ src/main/java/ro/home/project/config/WebMvcConfigurationSupport.java
@@ -1,20 +1,12 @@
 package ro.home.project.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
-import org.springframework.http.client.BufferingClientHttpRequestFactory;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import ro.home.project.web.filter.LoggerRequestInterceptorAdapter;
-import ro.home.project.web.filter.RequestResponseLoggingInterceptor;
-
-import java.util.Collections;
 
 @Configuration
 @RequiredArgsConstructor
